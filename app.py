7b1b07af8299200e7e3f2d1692a1c0f5aa85c769
Дима <63343338+Riv33R@users.noreply.github.com>
2024-11-15 21:33:01 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from flask import Flask, render_template, request
 
 import pandas as pd
 
+csvfile = "path to your data.csv"
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     # Чтение данных из CSV файла при каждом запросе
-    csv_file_path = 'userdata.csv'  # Путь к вашему CSV-файлу
+    csv_file_path = csvfile  # Путь к вашему CSV-файлу
     df = pd.read_csv(csv_file_path, encoding='utf-8')
     
     # Преобразуем данные в список словарей для передачи в шаблон
