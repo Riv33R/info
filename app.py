e6127a9eb3ff2328cc805992a106e96507838d42
Дима <63343338+Riv33R@users.noreply.github.com>
2024-11-15 21:30:14 +0000

    Add app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+import pandas as pd
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # Чтение данных из CSV файла при каждом запросе
+    csv_file_path = 'userdata.csv'  # Путь к вашему CSV-файлу
+    df = pd.read_csv(csv_file_path, encoding='utf-8')
+    
+    # Преобразуем данные в список словарей для передачи в шаблон
+    records = df.to_dict(orient='records')
+
+    return render_template('index.html', records=records)
+
+if __name__ == '__main__':
+
+    app.run(host="0.0.0.0", port=4000, debug=False)
+
