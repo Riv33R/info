a5bd901e96a6a1e7f8437be88cc14a59a04ba303
Дима <63343338+Riv33R@users.noreply.github.com>
2024-11-18 20:31:26 +0000

    Update script.js

--- static/script.js
+++ static/script.js
@@ -19,3 +19,25 @@ document.addEventListener("DOMContentLoaded", () => {
     // Перемещаем отсортированные строки обратно в `<tbody>`
     rows.forEach(row => tbody.appendChild(row));
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const themeToggle = document.getElementById("theme-toggle");
+    const body = document.body;
+
+    // Проверяем, есть ли сохранённая тема
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+      body.classList.add(savedTheme);
+    }
+
+    // Переключение темы
+    themeToggle.addEventListener("click", () => {
+      if (body.classList.contains("dark-theme")) {
+        body.classList.remove("dark-theme");
+        localStorage.setItem("theme", ""); // Очищаем тему
+      } else {
+        body.classList.add("dark-theme");
+        localStorage.setItem("theme", "dark-theme"); // Сохраняем тёмную тему
+      }
+    });
+  });
